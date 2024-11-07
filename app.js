f9f68f8833011549aa6d01fc30f539bce162e819
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2024-11-07 17:14:14 +0530

    server fixed to serve build files

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 // Visitor routes
 app.use("/api", visitorRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
