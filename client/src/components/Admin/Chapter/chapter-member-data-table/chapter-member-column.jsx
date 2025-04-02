c8a2e220c08c8dfc2d68feebe6ed14b76e3f3011
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2025-04-02 21:07:28 +0530

    Chapter layout Navigation Fixed

--- client/src/components/Admin/Chapter/chapter-member-data-table/chapter-member-column.jsx
+++ client/src/components/Admin/Chapter/chapter-member-data-table/chapter-member-column.jsx
@@ -153,7 +153,6 @@ export const MemberColumn = (allRoles) => [
         .split(',')
         .map((role) => role.trim())
         .filter((role) => role !== '');
-        console.log({ currentUserRoles });
 
       return (
         <div className="flex flex-wrap gap-2">
--- client/src/pages/Admin/chapters/AdminChapterLayout.jsx
+++ client/src/pages/Admin/chapters/AdminChapterLayout.jsx
@@ -1,54 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
+import { useLocation } from 'react-router-dom';
 
 const AdminChapterLayout = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const lastSegment = location.pathname.split('/').pop();
+  console.log('lastSegment', lastSegment);
+
   const [currentTab, setCurrentTab] = useState('Dashboard');
+
+  const list = [
+    {
+      name: 'Dashboard',
+      href: 'dashboard',
+    },
+    {
+      name: 'Member',
+      href: 'member',
+    },
+    {
+      name: 'Roles',
+      href: 'roles',
+    },
+  ];
+
+  useEffect(() => {
+    const foundTab = list.find((item) => item.href === lastSegment);
+    if (foundTab) {
+      setCurrentTab(foundTab.name);
+    } else {
+      setCurrentTab('Dashboard');
+    }
+  }, [lastSegment]);
 
   return (
     <div className="flex flex-col gap-4 p-4">
       {/* Navigation Tabs */}
       <Card className="p-4 flex gap-4">
-        <Button
-          variant="ghost"
-          className={`px-6 py-2 rounded-md ${
-            currentTab === 'Dashboard' ? 'bg-gray-200' : ''
-          }`}
-          onClick={() => {
-            setCurrentTab('Dashboard');
-            navigate('dashboard');
-          }}
-        >
-          Dashboard
-        </Button>
-
-        <Button
-          variant="ghost"
-          className={`px-6 py-2 rounded-md ${
-            currentTab === 'Member' ? 'bg-gray-200' : ''
-          }`}
-          onClick={() => {
-            setCurrentTab('Member');
-            navigate('member');
-          }}
-        >
-          Member
-        </Button>
-
-        <Button
-          variant="ghost"
-          className={`px-6 py-2 rounded-md ${
-            currentTab === 'Roles' ? 'bg-gray-200' : ''
-          }`}
-          onClick={() => {
-            setCurrentTab('Roles');
-            navigate('roles');
-          }}
-        >
-          Roles
-        </Button>
+        {list.map((item) => (
+          <Button
+            key={item.name}
+            variant="ghost"
+            className={`px-6 py-2 rounded-md ${
+              currentTab === item.name ? 'bg-gray-200' : ''
+            }`}
+            onClick={() => {
+              setCurrentTab(item.name);
+              navigate(item.href);
+            }}
+          >
+            {item.name}
+          </Button>
+        ))}
       </Card>
 
       {/* Dynamic Content Display */}
