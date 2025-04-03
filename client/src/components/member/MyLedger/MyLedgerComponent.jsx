a1cffaf2a9065eb5ef6e1449ba4cd33adb874e15
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2025-04-03 21:54:07 +0530

    member ledger completed

--- client/src/components/member/MyLedger/MyLedgerComponent.jsx
+++ client/src/components/member/MyLedger/MyLedgerComponent.jsx
@@ -5,6 +5,8 @@ import { useData } from '@/context/DataContext';
 import { axiosInstance } from '@/utils/config';
 import { MemberLedgerReportTable } from '../Reports/memberLedgerDataTable/MemberLedgerReportTable';
 import { MemberLedgerReportcolumn } from '../Reports/memberLedgerDataTable/MemberLedgerReportcolumn';
+import { Button } from '@/components/ui/button';
+import { toast } from 'react-toastify';
 
 const MyLedgerComponent = () => {
   const { chapterData } = useData();
@@ -25,6 +27,39 @@ const MyLedgerComponent = () => {
       });
   }, []);
 
+  const handleExportData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        `/api/report/${chapterData?.chapterId}/member-ledger`,
+        {
+          responseType: 'blob',
+        },
+      );
+
+      if (response.status !== 200) {
+        toast.error('Error exporting data');
+        return;
+      }
+
+      toast.success('Data exported successfully');
+
+      // Download blob
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute(
+        'download',
+        `${""} - ${new Date().toLocaleString()} - Member Ledger.xlsx`,
+      );
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    } catch (error) {
+      toast.error('Error exporting data');
+      console.error('Error exporting data:', error);
+    }
+  };
+
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       {error && (
@@ -33,6 +68,7 @@ const MyLedgerComponent = () => {
           <AlertDescription>{error}</AlertDescription>
         </Alert>
       )}
+      <Button onClick={handleExportData}>Export Data</Button>
       {loading ? (
         <Skeleton className="h-10 w-full mb-4" />
       ) : (
