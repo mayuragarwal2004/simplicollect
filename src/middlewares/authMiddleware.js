d2fe568f94a54b61f7d37e780941f6aa76957be2
Vallabh Sangvikar <vallabhdsangvikar18@gmail.com>
2025-05-18 19:59:27 +0530

    added AuthenticateAdmin middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
+const db = require("../config/db"); // Assuming you have a db.js file for database connection
 
 const authenticateToken = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
@@ -12,4 +13,18 @@ const authenticateToken = (req, res, next) => {
   });
 };
 
-module.exports = { authenticateToken };
+const AuthenticateAdmin =async (req, res, next) => {
+  if(!req.user){
+    return res.status(401).json({ message: "please authenticate first" });
+  }
+  const user = await db("members").where("memberId", req.user.memberId).first();
+  if (!user) {
+    return res.status(403).json({ message: "User not found" });
+  }
+  if (user.superAdmin==0) {
+    return res.status(403).json({ message: "Access denied, you need to be an Admin" });
+  }else{
+    next();
+  }
+};
+module.exports = { authenticateToken,AuthenticateAdmin };
